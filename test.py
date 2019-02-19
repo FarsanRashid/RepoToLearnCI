fc79596d730e972f461ea56c7f057fc75ceac0da
FarsanRashid <farsan.rashid@gmail.com>
2019-02-19 16:45:35 +0600

    Add all tests

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ class TestStringMethods(unittest.TestCase):
 	def test_add(self):
 		guru = math_class.math_guru()
 		self.assertEqual(guru.add(1 , 2), 3)
+	def test_sub(self):
+		guru = math_class.math_guru()
+		self.assertEqual(guru.subtract(2 , 1), 1)
+	def test_echo(self):
+		guru = math_class.math_guru()
+		self.assertEqual(guru.echo_name(), "math_guru")
 
 if __name__ == '__main__':
 	unittest.main()
